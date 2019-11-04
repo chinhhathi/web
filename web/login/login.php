a79fc5550a451670159288605112a5a68359b688
Chinh Ha <chinh201303@cvp.vn>
2019-11-05 02:20:30 +0700

    upload SCR1001

--- web/login/login.php
+++ web/login/login.php
@@ -12,15 +12,15 @@
 <body class="w3-content w3-black" style="max-width:1500px;">
     <header class="w3-display-container w3-center">
             <img class="w3-image w3-opacity" src="https://www.singhalonline.com/assets/images/photos/header-2.jpg" alt="Image 1" style="min-width:500px;" width="1500" height="1000">
-            <div class="w3-display-middle w3-padding w3-hide-small" style="width:50%">
-                <div class="w3-black w3-opacity w3-hover-opacity-off w3-padding-large w3-round-large" style="color: #A9A9A9;">
+            <div class="w3-display-middle w3-padding w3-hide-small " style="width:50%">
+                <div class="w3-black w3-padding-large w3-round-large" style="color: #A9A9A9;">
                     <h1>Sàn giao dịch Thực tập sinh</h1>
                     <hr class="w3-opacity">
-                    <div class="w3-black w3-opacity w3-hover-opacity-off w3-padding-large w3-round-large">
+                    <div class="w3-black w3-padding-large w3-round-large">
                         <div class="w3-bar">
-                            <button class="w3-bar-item w3-button w3-opacity" onclick="select_tab('student')">Log in as Student</button>
-                            <button class="w3-bar-item w3-button w3-opacity" onclick="select_tab('professor')">Log in as Professor</button>
-                            <button class="w3-bar-item w3-button w3-opacity" onclick="select_tab('company')">Log in as Company</button>
+                            <button class="w3-bar-item w3-button" onclick="select_tab('student')">Log in as Student</button>
+                            <button class="w3-bar-item w3-button" onclick="select_tab('professor')">Log in as Professor</button>
+                            <button class="w3-bar-item w3-button" onclick="select_tab('company')">Log in as Company</button>
                         </div>
 
                         <div id="student" class="w3-container types">
